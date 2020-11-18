513cffaee7f6769d7d8ec24e232172d65714fc24
Neeraj Surjaye <neerajsurjaye@gmail.com>
2020-11-19 00:27:38 +0530

    javascript update

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,10 @@ let noOfBooks = 0;
 let addBtn = document.querySelector("#add-book");
 let content = document.querySelector("#content");
 
+addBookToLibrary(new book("asadfsdf" , "asdf " , 43 ,50));
 addBookToLibrary(new book("asdf" , "asdf " , 43 ,345));
-addBookToLibrary(new book("asdf" , "asdf " , 43 ,345));
-addBookToLibrary(new book("asdf" , "asdf " , 43 ,345));
+addBookToLibrary(new book("asdddf" , "asdf " , 43 ,345));
 generateCard();
-
 let tempBook = new book("asdf" , "asdf " , 43 ,345);
 
 
@@ -29,7 +28,9 @@ function card(iBook){
     this.pages_read = document.createElement("p");
     this.addBtn = document.createElement("div");
     this.subBtn = document.createElement("div");
+    this.index = 0;
 
+    
     this.name.textContent = iBook.name;
     this.description.textContent = iBook.description;
     this.pages_read.textContent = `${iBook.pages_read} / ${iBook.total_pages}`;
@@ -44,6 +45,8 @@ function genHtml(obj){
     obj.right.append(obj.addBtn);
     obj.right.append(obj.pages_read);
     obj.right.append(obj.subBtn);
+    obj.index = noOfBooks;
+    noOfBooks++;
 
     obj.container.classList.add("container");
     obj.right.classList.add("right");
@@ -53,8 +56,46 @@ function genHtml(obj){
     obj.subBtn.textContent = "-";
     obj.addBtn.classList.add("primary-btn");
     obj.subBtn.classList.add("primary-btn");
+    obj.addBtn.id = "add";
+    obj.subBtn.id = "sub";
+    obj.pages_read.id = "pages-read";
+
+    obj.container.addEventListener("click" , function(e){
+        
+        update_pages(e , obj)
+    });
+
+}
+
+function update_pages(e , obj){
+    let index = 0;
+    
+    for(var i = 0 ; i < bookCards.length ; i++){
+        if(bookCards[i].index == obj.index){
+            break;
+        }
+        index++;
+    }
+
+    if(e.target.id =="add"){
+        if(mylibrary[index].pages_read+1 > mylibrary[index].total_pages){
+            alert("Err++");
+        }else{
+         mylibrary[index].pages_read++;
+        }
+    }else if(e.target.id =="sub"){
+        if(mylibrary[index].pages_read == 0){
+            alert("Err--");
+            
+        }else{
+            mylibrary[index].pages_read--;
+        }
+    }
 
 
+    obj.pages_read.textContent = `${mylibrary[index].pages_read} / ${mylibrary[index].total_pages}`;
+
+    console.log(mylibrary[index].pages_read);
 }
 
 
@@ -71,7 +112,6 @@ function generateCard(){
     for(var i = 0 ; i<mylibrary.length ; i++){
 
         bookCards[i] = new card(mylibrary[i]);
-        console.log(bookCards[i]);
         genHtml(bookCards[i]);
         content.append(bookCards[i].container);
 
