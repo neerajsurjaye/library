93902a19658511720aa7e7dbde18f02c344a72d1
Neeraj Surjaye <neerajsurjaye@gmail.com>
2020-11-22 14:52:45 +0530

    localstorage support

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,14 @@ let content = document.querySelector("#content");
 let form_close = document.querySelector("#close-button");
 let pop_form = document.querySelector("#pop-form");
 let add_book = document.querySelector("#add-book");
-let submit_button = document.querySelector("#form-submit")
+let submit_button = document.querySelector("#form-submit");
 
-addBookToLibrary(new book("asadfsdf" , "asdf " , 43 ,50));
-addBookToLibrary(new book("asdf" , "asdf " , 43 ,345));
-addBookToLibrary(new book("asdddf" , "asdf " , 43 ,345));
-generateCard();
+
+getBook();
+
+if(mylibrary.length != 0){
+    generateCard();
+}
 let tempBook = new book("asdf" , "asdf " , 43 ,345);
 
 
@@ -32,6 +34,7 @@ function card(iBook){
     this.pages_read = document.createElement("p");
     this.addBtn = document.createElement("div");
     this.subBtn = document.createElement("div");
+    this.cross = document.createElement("div");
     this.index = 0;
 
     
@@ -40,7 +43,7 @@ function card(iBook){
     this.pages_read.textContent = `${iBook.pages_read} / ${iBook.total_pages}`;
 }
 
-function genHtml(obj){
+function genHtml(obj , index){
     obj.left.append(obj.name);
     obj.left.append(obj.description);
     
@@ -49,12 +52,15 @@ function genHtml(obj){
     obj.right.append(obj.addBtn);
     obj.right.append(obj.pages_read);
     obj.right.append(obj.subBtn);
-    obj.index = noOfBooks;
+    obj.index = index;
+    obj.right.append(obj.cross);
     noOfBooks++;
 
     obj.container.classList.add("container");
     obj.right.classList.add("right");
     obj.left.classList.add("left");
+    obj.cross.id = "close-card";
+    obj.cross.textContent = "x";
 
     obj.addBtn.textContent = "+";
     obj.subBtn.textContent = "-";
@@ -65,8 +71,14 @@ function genHtml(obj){
     obj.pages_read.id = "pages-read";
 
     obj.container.addEventListener("click" , function(e){
-        
-        update_pages(e , obj)
+        if(e.target.id == "close-card"){
+            removeBook(obj);
+            return;
+        }
+
+        if(e.target.id == "add" || e.target.id == "sub"){
+            update_pages(e , obj)
+        }
     });
 
 }
@@ -98,13 +110,17 @@ function update_pages(e , obj){
 
 
     obj.pages_read.textContent = `${mylibrary[index].pages_read} / ${mylibrary[index].total_pages}`;
-
-    console.log(mylibrary[index].pages_read);
+    
+   
+    
+    setBook();
+    
 }
 
 
 function addBookToLibrary(iBook){
     mylibrary.push(iBook);
+    setBook();
 }
 
 
@@ -116,11 +132,12 @@ function generateCard(){
     bookCards = [];
     content.innerHTML = "";
     for(var i = 0 ; i<mylibrary.length ; i++){
+        if(mylibrary[i] != null){
 
-        bookCards[i] = new card(mylibrary[i]);
-        genHtml(bookCards[i]);
-        content.append(bookCards[i].container);
-
+            bookCards[i] = new card(mylibrary[i]);
+            genHtml(bookCards[i] , i);
+            content.append(bookCards[i].container);
+        }
         
     }
 
@@ -151,13 +168,36 @@ let iDescription = document.querySelector("#description");
 let iNoOfPage = document.querySelector("#no-of-pages");
 let iPagesRead = document.querySelector("#inp-pages-read");
 
-console.log(iDescription);
 
 function submit_book(e){
-    console.log(iDescription.value);   
+    
     let new_book = new book(iBookName.value , iDescription.value ,parseInt(iNoOfPage.value) ,parseInt(iPagesRead.value));
-    mylibrary.push(new_book);
+    addBookToLibrary(new_book);
     generateCard();
     pop_form.style.display = "none";
 }
 
+
+function getBook(){
+    if(localStorage.getItem("book-library") != null){
+        mylibrary = JSON.parse(localStorage.getItem("book-library"));
+        
+    }
+}
+
+function setBook(){
+    localStorage.setItem("book-library" , JSON.stringify(mylibrary));
+    console.log(mylibrary);
+}
+
+function removeBook(obj){
+    console.log(mylibrary);
+    console.log(obj.index);
+    mylibrary[obj.index] = null;
+    delete obj;
+    generateCard();
+    setBook();
+}
+
+
+//localStorage.removeItem("book-library");
